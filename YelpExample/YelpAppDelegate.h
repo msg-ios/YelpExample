fb9bbc01d37cd47d87664ff2b6ee4b75d4ff9957
msg-ios <msg.maruko@gmail.com>
2013-04-24 18:14:24 -0300

    Initial Commit

---
+++ YelpExample/YelpAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YelpAppDelegate.h
+//  YelpExample
+//
+//  Created by Marco S. Graciano on 4/24/13.
+//  Copyright (c) 2013 Marco Graciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YelpAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YelpExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  YelpExample
+//
+//  Created by Marco S. Graciano on 4/24/13.
+//  Copyright (c) 2013 Marco Graciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YelpAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YelpAppDelegate class]));
+    }
+}
